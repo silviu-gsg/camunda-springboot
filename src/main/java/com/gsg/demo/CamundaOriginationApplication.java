691f38720f47198629a7af07dcd8d4699a6cdb7a
Silviu Gheorghe <silviu.gheorghe@tremend.com>
2021-02-24 14:11:32 +0200

    initial commit with SpringBoot/Gradle module

---
+++ src/main/java/com/gsg/demo/CamundaOriginationApplication.java
@@ -0,0 +1,13 @@
+package com.gsg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaOriginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaOriginationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gsg/demo/CamundaOriginationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaOriginationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
